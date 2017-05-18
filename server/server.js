127a6b349a23bf257d9df0199fb56b765fc3dfc7
Jacques THÉRIEN <jacquestherien@ville.montreal.qc.ca>
2017-05-18 16:48:06 -0400

    io duplex

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
-
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () =>{
+io.on('connection', (socket) => {
+    console.log('nouvel usager');
+    socket.on('disconnect', ()=>{
+        console.log('Client déconnecté.');   
+    })
+});
+
+io.on('disconnect', () => {
+    console.log('Client déconnecté.');
+});
+
+server.listen(port, () =>{
     console.log(`Serveur démarré sur le port ${port}`);
 });
 
